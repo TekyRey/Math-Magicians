92fa02d40eed27e6259bb7a2e958b22b75031f36
TekyRey <mwakabayah@gmail.com>
2022-09-08 15:26:47 +0300

    Add logic to calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,40 +1,64 @@
 import React, { Component } from 'react';
+import calculate from '../logic/calculate.js';
 
 class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: null,
+      operator: null,
+    };
+  }
+
+  handleClick = (e) => {
+    const result = calculate(this.state, e.target.textContent);
+    this.setState(result);
+  };
+
+  resultDisplayClick = (e) => {
+    this.setState({
+      total: e.target.textContent,
+    });
+  };
+
   render() {
+    const { total, next, operation } = this.state;
     return (
       <div className="calculator-body">
-        <div className="result">
-          <p>0</p>
+        <div className="result" onChange={this.resultDisplayClick}>
+          <p>{total}
+            {operation}
+            {next}</p>
         </div>
         <div className="button">
-          <button name="AC" type="button">AC</button>
-          <button name="+/-" type="button">+/-</button>
-          <button name="%" type="button">%</button>
-          <button className='end' name="/" type="button">÷</button>
+          <button onClick={this.handleClick} name="AC" type="button">AC</button>
+          <button onClick={this.handleClick} name="+/-" type="button">+/-</button>
+          <button onClick={this.handleClick} name="%" type="button">%</button>
+          <button onClick={this.handleClick} className='end' name="/" type="button">÷</button>
           <br />
 
-          <button name="7" type="button">7</button>
-          <button name="8" type="button">8</button>
-          <button name="9" type="button">9</button>
-          <button className='end' name="x" type="button">x</button>
+          <button onClick={this.handleClick} name="7" type="button">7</button>
+          <button onClick={this.handleClick} name="8" type="button">8</button>
+          <button onClick={this.handleClick} name="9" type="button">9</button>
+          <button onClick={this.handleClick} className='end' name="x" type="button">x</button>
           <br />
 
-          <button name="4" type="button">4</button>
-          <button name="5" type="button">5</button>
-          <button name="6" type="button">6</button>
-          <button className='end' name="-" type="button">-</button>
+          <button onClick={this.handleClick} name="4" type="button">4</button>
+          <button onClick={this.handleClick} name="5" type="button">5</button>
+          <button onClick={this.handleClick} name="6" type="button">6</button>
+          <button onClick={this.handleClick} className='end' name="-" type="button">-</button>
           <br />
 
-          <button name="1" type="button">1</button>
-          <button name="2" type="button">2</button>
-          <button name="3" type="button">3</button>
-          <button className='end' name="+" type="button">+</button>
+          <button onClick={this.handleClick} name="1" type="button">1</button>
+          <button onClick={this.handleClick} name="2" type="button">2</button>
+          <button onClick={this.handleClick} name="3" type="button">3</button>
+          <button onClick={this.handleClick} className='end' name="+" type="button">+</button>
           <br />
 
-          <button className='btn' name="0" type="button">0</button>
-          <button name="." type="button">.</button>
-          <button className='end' name="=" type="button">=</button>
+          <button onClick={this.handleClick} className='btn' name="0" type="button">0</button>
+          <button onClick={this.handleClick} name="." type="button">.</button>
+          <button onClick={this.handleClick} className='end' name="=" type="button">=</button>
           <br />
         </div>
       </div>
